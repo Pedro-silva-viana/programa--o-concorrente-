388c13dd37b6cc6bbfd45d4d3d6385f6100e914a
Pedro-silva-viana <pedrovianaiguatu@gmail.com>
2024-03-01 07:49:13 -0300

    Create MPI13.c

---
+++ atividade 13 MPI/MPI13.c
@@ -0,0 +1,31 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 100
+
+float *gerar_vetor(int n);
+
+int main() {
+
+    //Iniciando MPI
+    
+    /*
+    FIM
+    */
+
+    MPI_Finalize();
+    return 0;
+}
+
+int *gerar_vetor_binario(int n) {
+  int *vetor;
+  int i; 
+  vetor = (int *)malloc(sizeof(int) * n);
+  for (i=0;i<n;i++) {
+    int num = rand() % 2;
+    vetor[i] = num;
+  }
+  return vetor;
+}
